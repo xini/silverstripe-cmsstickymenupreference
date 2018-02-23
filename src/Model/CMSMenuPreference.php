085a918b5af325edf8063e466d776c91b7afc110
Luke Fromhold <luke@fromhold.io>
2018-02-23 17:24:35 +1100

    Core class namespacing and upgrade

--- src/Model/CMSMenuPreference.php
+++ src/Model/CMSMenuPreference.php
@@ -1,24 +1,33 @@
 <?php
+
+namespace Innoweb\CMSStickyMenu\Model;
+
+use SilverStripe\Core\Extension;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Security\Security;
+use SilverStripe\Control\Cookie;
+
 class CMSMenuPreference extends Extension {
-    public function init() {
-        $mode = Config::inst()->get('UserMenuPreference', 'DefaultMode');
-        $userMode = Member::currentUser()->DefaultMenuMode;
-        
-        if(!empty($userMode)) {
-            $mode=$userMode;
+
+    public function init()
+    {
+        $mode = Config::inst()->get(UserMenuPreference::class, 'DefaultMode');
+        $userMode = Security::getCurrentUser()->DefaultMenuMode;
+
+        if (!empty($userMode)) {
+            $mode = $userMode;
         }
-		
-		if ($mode == 'default') {
-			Cookie::force_expiry('cms-menu-sticky', null, null, false, false);
-			Cookie::force_expiry('cms-panel-collapsed-cms-menu', null, null, false, false);
-		} else {
-			Cookie::set('cms-menu-sticky', 'true', 90, null, null, false, false);
-			if ($mode == 'open') {
-				Cookie::set('cms-panel-collapsed-cms-menu', 'false', 90, null, null, false, false);
-			} else {
-				Cookie::set('cms-panel-collapsed-cms-menu', 'true', 90, null, null, false, false);
-			}
-		}
-        
+
+        if ($mode == 'default') {
+            Cookie::force_expiry('cms-menu-sticky', null, null, false, false);
+            Cookie::force_expiry('cms-panel-collapsed-cms-menu', null, null, false, false);
+        } else {
+            Cookie::set('cms-menu-sticky', 'true', 90, null, null, false, false);
+            if ($mode == 'open') {
+                Cookie::set('cms-panel-collapsed-cms-menu', 'false', 90, null, null, false, false);
+            } else {
+                Cookie::set('cms-panel-collapsed-cms-menu', 'true', 90, null, null, false, false);
+            }
+        }
     }
 }
--- src/Model/UserMenuPreference.php
+++ src/Model/UserMenuPreference.php
@@ -1,60 +1,89 @@
 <?php
+
+namespace Innoweb\CMSStickyMenu\Model;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\OptionsetField;
+use SilverStripe\Control\Controller;
+
 class UserMenuPreference extends DataExtension {
-	
-    private static $db=array(
-		'DefaultMenuMode' => "Enum(array('open', 'closed', 'default'), 'default')"
-	);
-    
-    private static $defaults=array(
-		'DefaultMenuMode' => 'default'
-	);
-	
-    private $isSaving=false;
-    
-    
+
+    private static $db = [
+        'DefaultMenuMode'   =>  "Enum(array('open', 'closed', 'default'), 'default')"
+    ];
+
+    private static $defaults = [
+        'DefaultMenuMode'   =>  'default'
+    ];
+
+    protected $isSaving = false;
+
     /**
      * Updates the fields used in the cms
      * @param {FieldList} $fields Fields to be extended
      */
     public function updateCMSFields(FieldList $fields) {
-        
-        $fields->addFieldToTab(
-			'Root.Main', 
-			$field = OptionsetField::create(
-				'DefaultMenuMode',
-				_t('UserMenuPreference.MENU_MODE', '_Default Menu Mode'), 
-				array(
-					'open'=>_t('UserMenuPreference.OPEN_MODE', '_Open Mode: Menu is open and the sticky box is ticked'),
-					'closed'=>_t('UserMenuPreference.CLOSED_MODE', '_Closed Mode: Menu is closed and the sticky box is ticked'),
-					'preview'=>_t('UserMenuPreference.DEFAULT_MODE', '_Default Mode: Showing the menu is left to the CMS default behaviour')
-				),
-				Config::inst()->get('UserMenuPreference', 'DefaultMode')
-			)
-		);
-        
-        if(Session::get('ShowMenuSettingChangeReload')==true) {
-            $field->setError(_t('UserMenuPreference.CHANGE_REFRESH', '_You have updated your menu preference, you must refresh your browser to see the updated setting'), 'warning');
 
-            if($this->isSaving==false) {
-                Session::clear('ShowMenuSettingChangeReload');
+        $fields->addFieldToTab(
+            'Root.Main',
+            $field = OptionsetField::create(
+                'DefaultMenuMode',
+                _t('Innoweb\\CMSStickyMenu\\Model\\UserMenuPreference.MENU_MODE', '_Default Menu Mode'),
+                [
+                    'open'      =>  _t(
+                        'Innoweb\\CMSStickyMenu\\Model\\UserMenuPreference.OPEN_MODE',
+                        '_Open Mode: Menu is open and the sticky box is ticked'
+                    ),
+                    'closed'    =>  _t(
+                        'Innoweb\\CMSStickyMenu\\Model\\UserMenuPreference.CLOSED_MODE',
+                        '_Closed Mode: Menu is closed and the sticky box is ticked'
+                    ),
+                    'preview'   =>  _t(
+                        'Innoweb\\CMSStickyMenu\\Model\\UserMenuPreference.DEFAULT_MODE',
+                        '_Default Mode: Showing the menu is left to the CMS default behaviour'
+                    )
+                ],
+                Config::inst()->get(UserMenuPreference::class, 'DefaultMode')
+            )
+        );
+
+        $controller = Controller::curr();
+        $session = $controller->getRequest()->getSession();
+
+        if($session->get('ShowMenuSettingChangeReload') == true) {
+            $field->setMessage(
+                _t(
+                    'Innoweb\\CMSStickyMenu\\Model\\UserMenuPreference.CHANGE_REFRESH',
+                    '_You have updated your menu preference, you must refresh your browser to see the updated setting'
+                ),
+                'warning'
+            );
+
+            if ($this->isSaving == false) {
+                $session->clear('ShowMenuSettingChangeReload');
             }
         }
     }
-    
+
     /**
      * Ensures config defaults are enforced on write
-     */ 
-    public function onBeforeWrite() {
+     */
+    public function onBeforeWrite()
+    {
         parent::onBeforeWrite();
-        
-        if(empty($this->owner->DefaultMenuMode)) {
-            $this->owner->DefaultMenuMode = Config::inst()->get('UserMenuPreference', 'DefaultMode');
+
+        if (empty($this->owner->DefaultMenuMode)) {
+            $this->owner->DefaultMenuMode = Config::inst()->get(UserMenuPreference::class, 'DefaultMode');
         }
-                
-        //If changed ensure their is a session message
-        if($this->owner->isChanged('DefaultMenuMode')) {
-            Session::set('ShowMenuSettingChangeReload', true);
-            $this->isSaving=true;
+
+        // If changed ensure their is a session message
+        if ($this->owner->isChanged('DefaultMenuMode')) {
+            $controller = Controller::curr();
+            $session = $controller->getRequest()->getSession();
+            $session->set('ShowMenuSettingChangeReload', true);
+            $this->isSaving = true;
         }
     }
 }
